fee650a9d874cfd3ef2393bef55da6bfa7ce5bb1
Kshitij Gupta <guptakshitij2308@gmail.com>
2023-04-06 14:41:48 +0530

    Added types of facts component and rendered it

--- src/components/FactsCombined/FactCategories/FactCategories.jsx
+++ src/components/FactsCombined/FactCategories/FactCategories.jsx
@@ -1,8 +1,38 @@
 import React from "react";
 import "./Fact-Categories.css";
 
+const CATEGORIES = [
+  { name: "technology", color: "#3b82f6" },
+  { name: "science", color: "#16a34a" },
+  { name: "finance", color: "#ef4444" },
+  { name: "society", color: "#eab308" },
+  { name: "entertainment", color: "#db2777" },
+  { name: "health", color: "#14b8a6" },
+  { name: "history", color: "#f97316" },
+  { name: "news", color: "#8b5cf6" },
+];
+
 const FactCategories = () => {
-  return <aside>FactCategories</aside>;
+  return (
+    <aside>
+      <ul>
+        <li>
+          <button className="btn btn-all">All</button>
+        </li>
+
+        {CATEGORIES.map((cat) => (
+          <li key={cat.name}>
+            <button
+              style={{ backgroundColor: cat.color }}
+              className="btn btn-fact-type"
+            >
+              {cat.name}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </aside>
+  );
 };
 
 export default FactCategories;
