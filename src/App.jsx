6f29e05baa81899d3cb1921161fd0d211f112a7a
Kshitij Gupta <guptakshitij2308@gmail.com>
2023-04-07 19:15:43 +0530

    Added Loader to the facts

--- src/App.jsx
+++ src/App.jsx
@@ -5,48 +5,19 @@ import Form from "./components/Form/Form";
 import FactsCombined from "./components/FactsCombined/FactsCombined";
 import supabase from "./supabase";
 
-const initialFacts = [
-  {
-    id: 1,
-    text: "React is being developed by Meta (formerly facebook)",
-    source: "https://opensource.fb.com/",
-    category: "technology",
-    votesInteresting: 24,
-    votesMindblowing: 9,
-    votesFalse: 4,
-    createdIn: 2021,
-  },
-  {
-    id: 2,
-    text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
-    source:
-      "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
-    category: "society",
-    votesInteresting: 11,
-    votesMindblowing: 2,
-    votesFalse: 0,
-    createdIn: 2019,
-  },
-  {
-    id: 3,
-    text: "Lisbon is the capital of Portugal",
-    source: "https://en.wikipedia.org/wiki/Lisbon",
-    category: "society",
-    votesInteresting: 8,
-    votesMindblowing: 3,
-    votesFalse: 1,
-    createdIn: 2015,
-  },
-];
+// import Loading from "./components/Loading";
 
 function App() {
   const [showForm, setShowForm] = useState(false);
   const [facts, setFacts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(function () {
     async function getFacts() {
+      setIsLoading(true);
       const { data: fact, error } = await supabase.from("fact").select("*");
       setFacts(fact);
+      setIsLoading(false);
     }
     getFacts();
   }, []);
@@ -56,7 +27,12 @@ function App() {
       <Header showForm={showForm} setShowForm={setShowForm} />
       {showForm ? <Form setFacts={setFacts} setShowForm={setShowForm} /> : null}
 
-      <FactsCombined facts={facts} />
+      {isLoading ? (
+        <FactsCombined isLoading={isLoading} facts={facts} />
+      ) : (
+        <FactsCombined isLoading={isLoading} facts={facts} />
+      )}
+      {/* <FactsCombined facts={facts} /> */}
     </div>
   );
 }
--- src/components/FactsCombined/FactsCombined.jsx
+++ src/components/FactsCombined/FactsCombined.jsx
@@ -2,12 +2,14 @@ import React, { useState } from "react";
 
 import FactCategories from "./FactCategories/FactCategories";
 import FactList from "./FactList/FactList";
+import Loading from "../Loading";
 
-const FactsCombined = ({ facts }) => {
+const FactsCombined = ({ facts, isLoading }) => {
   return (
     <main className="fact-categories">
       <FactCategories />
-      <FactList facts={facts} />
+      {/* <FactList facts={facts} /> */}
+      {isLoading ? <Loading /> : <FactList facts={facts} />}
     </main>
   );
 };
